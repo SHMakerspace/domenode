1799f44802b6e4758af574854cc41bb650d0f41c
Nathan <nathan@inversesandwi.ch>
2018-08-23 23:15:45 +0100

    Laying foundation for EEPROM writing and reading

--- Firmware/NodeWare/NodeWare.ino
+++ Firmware/NodeWare/NodeWare.ino
@@ -13,23 +13,62 @@
 #include <ESPmDNS.h>
 
 // Set variables
+int nodeid;
 const char* wifi_ssid     = "emfcamp-insecure18";
 const char* wifi_password = "";
 String mdns_hostname = "";
-String ota_server = "firmware.shmakerspace.org";
+String ota_server = "http://firmware.shmakerspace.org";
 int ota_port = 80;
 String ota_location = "/esp/update/arduino.php";
 String ota_fwversion = "v0.1";
 
-int can_start() {
-  // Start the CAN bus at 500 kbps
-  if (CAN.begin(500E3)) {
-    Serial.println("[can] CAN started successfully at 500kbps!");
+// Set EEPROM location in filesystem
+EEPROMClass  NODEDATA("eeprom", 0x1000);
+
+void node_restart() {
+  // Snippet to restart gracefully
+  Serial.println("[node] Restarting!");
+  delay(5000);
+  ESP.restart();
+}
+
+int eeprom_start() {
+  // Start EEPROM and attempt to initialise
+  if (NODEDATA.begin(NODEDATA.length())) {
+    Serial.println("[eeprom] NODEDATA succesfully initialised!");
+    return 1;
+  }
+  else {
+    Serial.println("[eeprom] Failed to initialise NODEDATA partition!");
+    return 0;
+  }
+}
+
+int eeprom_checkid() {
+  // Check EEPROM for a node ID
+  NODEDATA.get(0, nodeid);
+  if (nodeid == -1) {
     return 0;
   }
   else {
-    Serial.println("[can] Starting CAN failed!");
     return 1;
+  }
+}
+
+int eeprom_setid() {
+  // Set a node ID in the EEPROM if one doesn't exist
+
+}
+
+int can_start() {
+  // Start the CAN peripheral at 500 kbps
+  if (CAN.begin(500E3)) {
+    Serial.println("[can] CAN started successfully at 500kbps!");
+    return 1;
+  }
+  else {
+    Serial.println("[can] Starting CAN failed!");
+    return 0;
   }
 }
 
@@ -42,24 +81,25 @@ int wifi_start(char* ssid, char* password) {
     Serial.print(".");
   }
   Serial.println("[wifi] Successfully connectected to network!");
-  return 0;
+  return 1;
 }
 
-int mdns_start(hostname) {
+int mdns_start(const char* hostname) {
   // Start mDNS responder
   if (MDNS.begin(hostname)) {
     Serial.println("[mdns] mDNS responder started");
-    return 0;
+    return 1;
   }
   else {
     Serial.println("[mdns] Starting MDNS responder failed!");
-    return 1;
+    return 0;
   }
 }
 
-int ota_start(server, port, location, fwversion) {
+/* Commented out to compile
+  int ota_start(String server, String port, String location, String fwversion) {
   // Check for firmware updates
-  t_httpUpdate_return ret = ESPhttpUpdate.update(server, port, location, fwversion);
+  //t_httpUpdate_return ret = ESPhttpUpdate.update(server, port, location, fwversion);
   switch (ret) {
     case HTTP_UPDATE_FAILED:
       Serial.println("[ota] Update failed!");
@@ -71,13 +111,38 @@ int ota_start(server, port, location, fwversion) {
       Serial.println("[ota] Update found! Starting firmware update ");
       break;
   }
-}
+  }
+*/
 
 void setup() {
   // Start serial communication for debug
   Serial.begin(115200);
-  Serial.println("[node] DomeNode #01 running v0.1");
+  Serial.println("[node] Booting...");
 
+  // Start EEPROM
+  if (!eeprom_start()) {
+    node_restart();
+  }
+
+  if (eeprom_checkid()) {
+    Serial.println("[eeprom] Found node ID in EEPROM!");
+    Serial.print("[node] This is node ");
+    Serial.println(nodeid);
+  }
+  else {
+    Serial.println("[eeprom] Failed finding node ID in EEPROM!");
+    Serial.println("[node] Please set node ID!");
+    node_restart();
+  }
+
+  Serial.println(nodeid);
+
+  // Start CAN communication
+  if (!can_start()) {
+    node_restart();
+  }
+
+  //
 }
 
 void loop() {
--- Firmware/NodeWare/readme.md
+++ Firmware/NodeWare/readme.md
@@ -2,5 +2,6 @@
 The firmware for the DomeNodes is written in Arduino for the ESP32 microcontroller platform
 
 ## Dependencies
-* [Arduino core for ESP32 WiFi chip](https://github.com/espressif/arduino-esp32)
-* [Arduino CAN](https://github.com/sandeepmistry/arduino-CAN)
+* [Arduino core for ESP32 WiFi chip](https://github.com/espressif/arduino-esp32) - install via Arduino IDE boards manager
+* [Arduino CAN](https://github.com/sandeepmistry/arduino-CAN) - install via Arduino IDE libraries manager
+* [ESP32 Partition Manager](https://github.com/francis94c/ESP32Partitions) - requires Python 2.7
