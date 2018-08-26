17c73d36ae51a633d039cf9de8bc4c5a8f938c88
Nathan <nathan@inversesandwi.ch>
2018-08-26 15:25:45 +0100

    Added touch capability and started on CAN messages

--- Firmware/NodeWare/NodeWare.ino
+++ Firmware/NodeWare/NodeWare.ino
@@ -17,7 +17,7 @@
 bool node_debug = true; // Enable this to prevent the ESP from restarting on an error to aid debugging
 
 // Set variables
-int node_id; // Leave this varible empty to read from EEPROM at boot, or set a value and it'll be written to the EEPROM
+int node_id = 1; // Leave this varible empty to read from EEPROM at boot, or set a value and it'll be written to the EEPROM
 uint64_t node_mac;
 int node_fwversion ;
 const char* wifi_ssid     = "emfcamp-insecure18";
@@ -27,6 +27,9 @@ String ota_server = "http://firmware.shmakerspace.org";
 int ota_port = 80;
 String ota_location = "/domenode/update/ota.php";
 String ota_fwversion = "";
+
+bool touch_detected = false;
+int touch_threshold = 40;
 
 // Define non-volatile storage
 Preferences preferences;
@@ -109,11 +112,46 @@ int can_start() {
   // Start the CAN peripheral at 500 kbps
   if (CAN.begin(500E3)) {
     Serial.println("[can] CAN started successfully at 500kbps!");
+    CAN.onReceive(can_recievecallback);
     return 1;
   }
   else {
     Serial.println("[can] Starting CAN failed!");
     return 0;
+  }
+}
+
+int can_sendpacket(byte function, char message) {
+  byte frameid;
+  // TODO
+  CAN.beginPacket(frameid);
+  CAN.endPacket();
+  return 1;
+}
+
+void can_recievecallback(int packetSize) {
+  // received a packet
+  Serial.print("[can] Received ");
+  if (CAN.packetExtended()) {
+    Serial.print("extended ");
+  }
+  if (CAN.packetRtr()) {
+    // Remote transmission request, packet contains no data
+    Serial.print("RTR ");
+  }
+  Serial.print("packet with id 0x");
+  Serial.print(CAN.packetId(), HEX);
+  if (CAN.packetRtr()) {
+    Serial.print(" and requested length ");
+    Serial.println(CAN.packetDlc());
+  }
+  else {
+    Serial.print(" and length ");
+    Serial.println(packetSize);
+    // only print packet data for non-RTR packets
+    while (CAN.available()) {
+      Serial.println((char)CAN.read());
+    }
   }
 }
 
@@ -141,6 +179,9 @@ int mdns_start(const char* hostname) {
   }
 }
 
+void touch_interrupt() {
+  touch_detected = true;
+}
 
 /* Commented out to compile
   int ota_start(String server, String port, String location, String fwversion) {
@@ -195,10 +236,21 @@ void setup() {
     node_restart();
   }
 
+  // Attach capative ring sense to an interrupt
+  Serial.println("[touch] Attaching interrupt to capacitive sensor 8...");
+  touchAttachInterrupt(T8, touch_interrupt, touch_threshold);
+
   // Initialise pixels and specify total number and GPIO pin
   //pixels_start(9, 17);
 }
 
 void loop() {
-
+  if (touch_detected) {
+    touch_detected = false;
+    Serial.println("[touch] Ring touched!");
+    Serial.println("[can] Sending CAN packet with node ID 0000001, function code 0000...");
+    CAN.beginPacket(0x01); // Node ID 000001, Function code 0000 (testing only)
+    CAN.write(1);
+    CAN.endPacket();
+  }
 }
