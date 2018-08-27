a1877fb82b3761f11d1d53d3cf7ab27379d08cf0
Nathan <nathan@inversesandwi.ch>
2018-08-27 16:03:13 +0100

    Added heartbeat LED, Arduino OTA, tidied up code a little

--- Firmware/NodeWare/NodeWare.ino
+++ Firmware/NodeWare/NodeWare.ino
@@ -7,27 +7,44 @@
 
 // Include the libraries we require
 #include <Preferences.h>
+#include <Ticker.h>
 #include <CAN.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
 #include <ESPmDNS.h>
+#include <ArduinoOTA.h>
 #include <NeoPixelBus.h>
+#include <NeoPixelAnimator.h>
+
+// Define pin numbers
+#define pin_heartbeat LED_BUILTIN
+#define pin_touch T8
 
 // Debug variables
 bool node_debug = true; // Enable this to prevent the ESP from restarting on an error to aid debugging
 
 // Set variables
-int node_id = 1; // Leave this varible empty to read from EEPROM at boot, or set a value and it'll be written to the EEPROM
+// * Node variables
+int node_id; // Leave this varible empty to read from EEPROM at boot, or set a value and it'll be written to the EEPROM
 uint64_t node_mac;
 int node_fwversion ;
-const char* wifi_ssid     = "emfcamp-insecure18";
-const char* wifi_password = "";
+// * WiFi variables
+char* wifi_ssid     = "";
+char* wifi_password = "";
+// * mDNS variables
 String mdns_hostname = "shm-domenode-";
+char mdns_hostnamebuffer[32];
+// * OTA variables
 String ota_server = "http://firmware.shmakerspace.org";
 int ota_port = 80;
 String ota_location = "/domenode/update/ota.php";
 String ota_fwversion = "";
-
+// * Heartbeat variables
+Ticker heartbeat_blinker;
+Ticker heartbeat_toggler;
+float heartbeat_pace = 0.05;  //seconds
+const float heartbeat_period = 2; //seconds
+// * Touch variables
 bool touch_detected = false;
 int touch_threshold = 40;
 
@@ -48,11 +65,32 @@ void node_restart() {
   return;
 }
 
-int node_printmac() {
-  node_mac = ESP.getEfuseMac();
-  Serial.printf("[node] MAC Address: %04X", (uint16_t)(node_mac >> 32)); // Print high 2 bytes
-  Serial.printf("%08X\n", (uint32_t)node_mac); // Print low 4 bytes.
+int node_printmac(uint64_t mac) {
+  Serial.printf("[node] MAC Address: %04X", (uint16_t)(mac >> 32)); // Print high 2 bytes
+  Serial.printf("%08X\n", (uint32_t)mac); // Print low 4 bytes.
   return 1;
+}
+
+void heartbeat_start() {
+  pinMode(pin_heartbeat, OUTPUT);
+  heartbeat_toggler.attach(heartbeat_period, heartbeat_toggle);
+}
+
+void heartbeat_blink() {
+  digitalWrite(pin_heartbeat, !digitalRead(pin_heartbeat));
+}
+
+void heartbeat_toggle() {
+  static bool heartbeat_blinkingstatus = false;
+  if (heartbeat_blinkingstatus) {
+    heartbeat_blinker.detach();
+    heartbeat_blinkingstatus = false;
+  }
+  else {
+    heartbeat_blinker.attach(heartbeat_pace, heartbeat_blink);
+    heartbeat_blinkingstatus = true;
+  }
+  digitalWrite(pin_heartbeat, LOW);  //make sure LED on on after toggling (pin LOW = led ON)
 }
 
 int nvs_start() {
@@ -122,10 +160,7 @@ int can_start() {
 }
 
 int can_sendpacket(byte function, char message) {
-  byte frameid;
   // TODO
-  CAN.beginPacket(frameid);
-  CAN.endPacket();
   return 1;
 }
 
@@ -157,13 +192,16 @@ void can_recievecallback(int packetSize) {
 
 int wifi_start(char* ssid, char* password) {
   // Start WiFi client and connect to predefined SSID
-  Serial.println("[wifi] Attemping to connection to network");
+  Serial.print("[wifi] Attemping to connection to network");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("[wifi] Successfully connectected to network!");
+  Serial.println("");
+  Serial.println("[wifi] Successfully connected to network!");
+  Serial.print("[wifi] IP address: ");
+  Serial.println(WiFi.localIP());
   return 1;
 }
 
@@ -183,8 +221,39 @@ void touch_interrupt() {
   touch_detected = true;
 }
 
+int ota_arduino_start() {
+  mdns_hostname.toCharArray(mdns_hostnamebuffer, 32);
+  ArduinoOTA.setHostname(mdns_hostnamebuffer);
+  ArduinoOTA.setPasswordHash("b413dd8e153df6ad2938814c7858860c"); //hackmeplease
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("[ota] Update invitation recieved, update type is " + type + "!");
+  })
+  .onEnd([]() {
+    Serial.println("\n [ota] Update download finished!");
+  })
+  .onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("[ota] Update progress: %u%%\r", (progress / (total / 100)));
+  })
+  .onError([](ota_error_t error) {
+    Serial.printf("[ota] Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Authentication failed!");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Couldn't start!");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connection failed!");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Download failed!");
+    else if (error == OTA_END_ERROR) Serial.println("Couldn't finish!");
+  });
+  ArduinoOTA.begin();
+}
+
 /* Commented out to compile
-  int ota_start(String server, String port, String location, String fwversion) {
+  int ota_http_start(String server, String port, String location, String fwversion) {
   // Check for firmware updates
   //t_httpUpdate_return ret = ESPhttpUpdate.update(server, port, location, fwversion);
   switch (ret) {
@@ -201,12 +270,15 @@ void touch_interrupt() {
   }
 */
 
-int pixels_start(int count, int pin) {
+/*
+  int pixels_start(int count, int pin) {
   // Initilise pixels
-  NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> pixels(count, pin);
+  pixels.Begin();
+  pixels.Show();
   Serial.println("[pixels] Successfully initialised pixels!");
   return 1;
-}
+  }
+*/
 
 void setup() {
   // Start serial communication for debug
@@ -216,8 +288,12 @@ void setup() {
     Serial.println("[node] DEBUG MODE ENABLED!");
   }
 
+  // Start heartbeat
+  Serial.println("[heartbeat] Starting heartbeat...");
+  heartbeat_start();
+
   // Print MAC address of the ESP32
-  node_printmac();
+  node_printmac(ESP.getEfuseMac());
 
   // Start NVS filesystem and prep ID
   nvs_start();
@@ -230,27 +306,37 @@ void setup() {
     Serial.println("[node] Please set ID in firmware!");
     node_restart();
   }
+  // Append node ID to hostname
+  Serial.println("[mdns] Setting node hostname...");
+  mdns_hostname += String(node_id);
 
   // Start CAN communication
   if (!can_start()) {
     node_restart();
   }
 
-  // Attach capative ring sense to an interrupt
-  Serial.println("[touch] Attaching interrupt to capacitive sensor 8...");
-  touchAttachInterrupt(T8, touch_interrupt, touch_threshold);
+  // Start WiFi communication
+  wifi_start(wifi_ssid, wifi_password);
 
-  // Initialise pixels and specify total number and GPIO pin
-  //pixels_start(9, 17);
+  // Setup OTA updating (via IDE) and advertise service over network
+  ota_arduino_start();
+
+  // Attach capative ring sense to an interrupt
+  Serial.println("[touch] Attaching interrupt to capacitive sensor...");
+  touchAttachInterrupt(pin_touch, touch_interrupt, touch_threshold);
 }
 
 void loop() {
+  // Check if OTA invitation has been recieved
+  ArduinoOTA.handle();
+
+  // Check if touch interrupt has been triggered
   if (touch_detected) {
     touch_detected = false;
-    Serial.println("[touch] Ring touched!");
-    Serial.println("[can] Sending CAN packet with node ID 0000001, function code 0000...");
-    CAN.beginPacket(0x01); // Node ID 000001, Function code 0000 (testing only)
-    CAN.write(1);
+    Serial.println("[touch] Interrupt triggered!");
+    Serial.println("[can] Sending CAN packet with node ID 0000001, function code 0001...");
+    CAN.beginPacket(0x81); // Node ID 000001, Function code 0001 (testing only)
+    CAN.write(true);
     CAN.endPacket();
   }
 }
